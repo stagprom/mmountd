ede7b0a72f4ecac510288775e40c77890fed7038
stagprom <stagprom@posteo.de>
2015-11-22 14:21:50 +0100

    Updated copyright

--- src/log.c
+++ src/log.c
@@ -1,24 +1,28 @@
-/******************************************************************************
-Minimalistic (auto) mount daemon
-Copyright (C) 2015  stagprom
-
-This file is part of mmountd.
-
-mmountd is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-mmountd is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with mmountd.  If not, see <http://www.gnu.org/licenses/>.
-
-********************************************************************************/
-
+/*
+ *  Minimalistic (auto) mount daemon
+ *  Copyright (C) 2015  Ernest Vogelsang <stagprom@posteo.de>
+ * 
+ *  This file is part of mmountd.
+ * 
+ *  mmountd is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  mmountd is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with mmountd.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  Original provided by fon.com
+ *  Copyright (C) 2009 John Crispin <blogic@openwrt.org> 
+ *
+ */
+ 
+ 
 #include <stdio.h>
 #include <syslog.h>
 #include <stdarg.h>
